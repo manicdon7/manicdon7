a35b2bd36d64ededc016d96a6844ed6ccf987c35
manicdon7 <71627991+manicdon7@users.noreply.github.com>
2022-02-21 20:44:32 +0530

    Add files via upload

---
+++ signin.py
@@ -0,0 +1,9 @@
+username = input("Enter username:")
+password = input("Enter password:")
+print("Your are signed up")
+username1 = input("Enter username:")
+password1 = input("Enter password:")
+if username == username1 and password == password1:
+    print("you are logged in")
+else:
+    print("invalid credentials")
